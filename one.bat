4d5ffb8c488528bd04065b4c8d9f1c6fb56863bf
Carlos Rabelo <contato@carlosrabelo.com.br>
2024-05-18 10:25:47 -0400

    Initial commit

---
+++ one.bat
@@ -0,0 +1,43 @@
+@echo off
+
+: Xbox Applications
+winget uninstall Microsoft.GamingApp_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxApp_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.Xbox.TCUI_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxSpeechToTextOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxIdentityProvider_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxGamingOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+winget uninstall Microsoft.XboxGameOverlay_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: 3D Viewer
+winget uninstall 9NBLGGH42THS --accept-source-agreements --silent
+
+: Feedback Hub
+winget uninstall Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Groove Music
+winget uninstall Microsoft.ZuneMusic_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: MS Maps
+winget uninstall Microsoft.WindowsMaps_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: MS Pay 
+winget uninstall Microsoft.Wallet_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Microsoft-Tips...
+winget uninstall Microsoft.Getstarted_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Mixed Reality-Portal
+winget uninstall Microsoft.MixedReality.Portal_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Paint-3D...
+winget uninstall 9NBLGGH5FV99 --accept-source-agreements --silent
+
+: People
+winget uninstall Microsoft.People_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Your Phone
+winget uninstall Microsoft.YourPhone_8wekyb3d8bbwe --accept-source-agreements --silent
+
+: Sticky Notes...
+winget uninstall Microsoft.MicrosoftStickyNotes_8wekyb3d8bbwe --accept-source-agreements --silent
